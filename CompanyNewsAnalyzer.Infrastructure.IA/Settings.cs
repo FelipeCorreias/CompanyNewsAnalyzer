c7111890ac5f82dc824a968df9b5b2f296c825cb
FelipeCorreias <felipe@fcorreia.com.br>
2019-04-02 13:14:58 -0300

    Update Settings.cs

--- CompanyNewsAnalyzer.Infrastructure.IA/Settings.cs
+++ CompanyNewsAnalyzer.Infrastructure.IA/Settings.cs
@@ -4,7 +4,7 @@ namespace CompanyNewsAnalyzer.Infrastructure.IA
 {
     public class Settings
     {
-        public const string _apiKey = "Ucun8-Q4TM3IcitZjo55zZGG-e9lFszV35vWB7zgwn7W";
+        public const string _apiKey = "{APIKEY}";
         public const string _url = "https://gateway-wdc.watsonplatform.net/natural-language-understanding/api";
         public const string _versionDate = "2018-03-19";
 
