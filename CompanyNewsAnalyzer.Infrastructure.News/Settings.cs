8994c68ff9c922c09331abc3140822b376da6423
FelipeCorreias <felipe@fcorreia.com.br>
2019-04-02 13:15:24 -0300

    Update Settings.cs

--- CompanyNewsAnalyzer.Infrastructure.News/Settings.cs
+++ CompanyNewsAnalyzer.Infrastructure.News/Settings.cs
@@ -6,7 +6,7 @@ namespace CompanyNewsAnalyzer.Infrastructure.News
 {
     public class Settings
     {
-        public const string _apiKey = "dcb3ea6ffc12430d97af658cd1528eb5";
+        public const string _apiKey = "{APIKEY}";
         public const string _url = "https://newsapi.org/v2/";
 
         public string ApiKey => _apiKey;
